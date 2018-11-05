3b208b8f025b47f32bd343d5d0b0e8fc746cb96b
Alastair Yu <alastair.yu@hotmail.com>
2018-11-05 03:36:37 +0000

    updates

--- models.py
+++ models.py
@@ -38,6 +38,11 @@ class ModelLoader():
                            metrics=metrics)
         
         print(self.model.summary())
+    
+    def normalization(self, input_tensor):
+        output = Lambda(lambda x: x/255.0 - 0.5, input_shape=self.input_shape)(input_tensor)
+        output = Cropping2D(cropping=((70, 25), (0,0)))(output)
+        return output
 
     def multiple_output(self):
         kernel_size = (3, 3)
@@ -46,14 +51,24 @@ class ModelLoader():
 
         input_img = Input(shape=self.input_shape)
 
+        # preprocessing
+        angle = self.normalization(input_img)
+        
         angle = Conv2D(32, kernel_size, activation='relu', padding='same')(input_img)
         angle = MaxPooling2D(pool_size=pool_size)(angle)
+        angle = Dropout(0.25)(angle)
 
         angle = Conv2D(64, kernel_size, activation='relu', padding='same')(angle)
         angle = MaxPooling2D(pool_size=pool_size)(angle)
+        angle = Dropout(0.25)(angle)
+                            
+        angle = Conv2D(128, kernel_size, activation='relu', padding='same')(angle)
+        angle = MaxPooling2D(pool_size=pool_size)(angle)
+        angle = Dropout(0.25)(angle)
 
         angle = Flatten()(angle)
         angle = Dense(128, init='normal', activation='relu')(angle)
+        angle = Dropout(0.25)(angle)
         angle = Dense(1, init='normal', activation='linear')(angle)
 
         model = Model(inputs=input_img, outputs=angle)
--- train.py
+++ train.py
@@ -22,27 +22,30 @@ def load_images(csv_path, img_path, expected_shape):
     measurements = []
 
     for line in lines:
-        source_path = line[0]
-        filename = source_path.split('/')[-1]
+        center_image = np.array(cv2.imread(img_path +  line[0].split('/')[-1]))
+#         left_image   = np.array(cv2.imread(img_path +  line[1].split('/')[-1]))
+#         right_image  = np.array(cv2.imread(img_path +  line[2].split('/')[-1]))
 
-        current_path = img_path + filename
-
-        image = np.array(cv2.imread(current_path))
-
-        if image.shape != expected_shape:
+#         if (center_image.shape != expected_shape) or (left_image.shape != expected_shape) or (right_image.shape != expected_shape):
+#             continue
+        if center_image.shape != expected_shape:
             continue
 
-        # print("reading image: " + current_path)
-
-        images.append(image)
-        # print(image)
-
+        images.append(center_image)
         measurement = float(line[3])
-        # print("adding measurements: " + str(measurements))
+        print(measurement)
         measurements.append(measurement)
 
-    X_train = np.array(images)
-    y_train = np.array(measurements)
+    aug_images, aug_measurements = [], []
+    for image, measurement in zip(images, measurements):
+        aug_images.append(image)
+        aug_measurements.append(measurement)
+        
+        aug_images.append(cv2.flip(image, 1))
+        aug_measurements.append(measurement*-1.0)
+        
+    X_train = np.array(aug_images)
+    y_train = np.array(aug_measurements)
 
     print(X_train.shape, y_train.shape)
 
@@ -57,8 +60,8 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    CSV_PATH = "/home/alastair/git_projects/udacity/CarND-Behavioral-Cloning-P3/data/driving_log.csv"
-    IMG_PATH = "/home/alastair/git_projects/udacity/CarND-Behavioral-Cloning-P3/data/IMG/"
+    CSV_PATH = "./data/driving_log.csv"
+    IMG_PATH = "./data/IMG/"
     img_shape = (160, 320, 3)
 
     X_train, y_train = load_images(CSV_PATH, IMG_PATH, img_shape)
@@ -66,7 +69,7 @@ if __name__ == "__main__":
     
     early_stopper = EarlyStopping(patience=2)
     checkpointer = ModelCheckpoint(
-        filepath="checkpoints/" + args.model_name + '-{epoch:03d}-{loss:.3f}.hdf5',
+        filepath="checkpoints/" + args.model_name + '.hdf5',
         verbose=1,
         save_best_only=True)
 
